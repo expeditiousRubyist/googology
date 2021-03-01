4826ed76ac548294ec75fe9ad6acf0e4ae21c5b0
rubyist <rubyist.personal@openmailbox.org>
2021-02-28 21:26:21 -0800

    Add support for multiple long scales

--- src/conway_wechsler.rs
+++ src/conway_wechsler.rs
@@ -3,10 +3,13 @@
 //! [this website](http://www.mrob.com/pub/math/largenum.html#conway-wechsler).
 //! 
 //! We add to this system just slightly by allowing the user to choose between
-//! the short scale (thousand, million, billion, trillion...), which is the
-//! standard in modern English, and the long scale (thousand, million, milliard,
-//! billion...), which is more common in European languages, and which used to
-//! be the standard for British English.
+//! three different scales. The short scale (thousand, million, billion,
+//! trillion, ...) is the standard in modern English, while the long scale
+//! (thousand, million, milliard, billion, ...) is more common in European
+//! languages. It is also possible to use a variant of the long scale previously
+//! used in the UK before switching to the short scale, where instead of using
+//! milliard to refer to the value 10^9, the term "one thousand million" is used
+//! instead.
 
 extern crate num_traits;
 extern crate num_bigint;
@@ -26,6 +29,22 @@ use crate::common::{
 
 use crate::ParseError;
 
+/// A parameter for Conway-Wechsler functions which indicates how number names
+/// change every power of 1000.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Scale {
+	/// The scale typically used in modern English-speaking nations.
+	/// 10^9 will be called one billion.
+	Short,
+	/// The scale formerly used in the UK, and based on Chuquet's
+	/// scheme for naming numbers. 10^9 will be called one thousand million.
+	LongBritish,
+	/// The scale currently in use by many European languages, most
+	/// notably French. 10^9 will be called one milliard.
+	LongPeletier,
+}
+
+
 // Create a name for a single 3 digit zillion number, ending in -illi.
 // Value for zero is "nilli", for use in chained zillion numbers.
 // Values above 999 will panic.
@@ -41,26 +60,35 @@ fn zillion_prefix(num: usize) -> Result<String, ParseError> {
 // Value for anything greater will involve repeated application of the
 // zillion_prefix function, to create a number ending in "illion",
 // or "ard" depending on whether or not we are using the long scale.
-fn zillion_number(num: usize, short: bool) -> Result<String, ParseError> {
+fn zillion_number(num: usize, scale: Scale) -> Result<String, ParseError> {
 	if num == 0 { return Ok(String::from("")); }
 	if num == 1 { return Ok(String::from("thousand")); }
-	
-	let mut name   = String::from("");
-	let mut power  = num - 1;
-	let mut suffix = "on";
 
-	// Adjust for long scale
-	if !short {
-		if num % 2 != 0 { suffix = "ard"; }
-		power = ((num + 2) / 2) - 1;
+	// Create adjustments to name for long scale.
+	let (prefix, suffix) = match (scale, num % 2) {
+		(Scale::LongBritish, 1)  => ("thousand ", "on"),
+		(Scale::LongPeletier, 1) => ("", "ard"),
+		(_, _) => ("", "on"),
+	};
+
+	let mut power = match scale {
+		Scale::Short => num - 1,
+		_ => ((num + 2) / 2) - 1,
+	};
+
+	let mut name = String::from(prefix);
+
+	// Zillion prefixes added in reverse order here.
+	// e.g. in millinillion, first add "nilli", then "milli", then "on".
+	let mut zillions = Vec::with_capacity(7);
+	while power > 0 {
+		let zillion = zillion_prefix(power % 1000)?;
+		zillions.push(zillion);
+		power /= 1000;
 	}
 
-	// Prefixes technically added in reverse order here.
-	// e.g. in millinillion, first add "nilli", then "milli", then "on".
-	while power > 0 {
-		let prefix = zillion_prefix(power % 1000)?;
-		name.insert_str(0, prefix.as_str());
-		power /= 1000;
+	for z in zillions.iter().rev() {
+		name.push_str(z.as_str());
 	}
 
 	name.push_str(suffix);
@@ -75,21 +103,20 @@ fn zillion_number(num: usize, short: bool) -> Result<String, ParseError> {
 /// * `digits` - A string slice that holds a representation of the number
 /// using only the digits 0-9. If any other character is present, this function
 /// will return an Err.
-/// * `short` - A boolean to determine whether or not the short scale should
-/// be used. For reference, the represented by `10^9` is called "one billion"
-/// using the short scale. When `short` is set to false, the long scale is used
-/// instead, and this number would be called "one milliard".
+/// * `scale` - An enumerated value to determine which scale should
+/// be used. Short scales use a new "-illion" name for every power of 1000,
+/// while long scales use a new "-illion" name for every power of 1000000.
 /// 
 /// # Example
 /// 
 /// ```
-/// use googology::conway_wechsler::full_name;
-/// let milliard = full_name("19000000042", false).unwrap();
-/// let billion = full_name("19000000042", true).unwrap();
+/// use googology::conway_wechsler::{Scale, full_name};
+/// let milliard = full_name("19000000042", Scale::LongPeletier).unwrap();
+/// let billion = full_name("19000000042", Scale::Short).unwrap();
 /// assert_eq!("nineteen milliard forty two", milliard.as_str());
 /// assert_eq!("nineteen billion forty two", billion.as_str());
 /// ```
-pub fn full_name(digits: &str, short: bool) -> Result<String, ParseError> {
+pub fn full_name(digits: &str, scale: Scale) -> Result<String, ParseError> {
 	// Sanity checks. We want the string to be entirely digits, and we want
 	// to handle the case of leading zeroes. If all digits are zero, we want
 	// to just return the string "zero", and otherwise process from the
@@ -117,7 +144,7 @@ pub fn full_name(digits: &str, short: bool) -> Result<String, ParseError> {
 	if first > 0 {
 		let num     = num_from_slice(digits, i, first);
 		let leading = myriad_number(num)?;
-		let zillion = zillion_number(remaining / 3, short)?;
+		let zillion = zillion_number(remaining / 3, scale)?;
 
 		output.push_str(leading.as_str());
 		if !zillion.is_empty() {
@@ -133,7 +160,7 @@ pub fn full_name(digits: &str, short: bool) -> Result<String, ParseError> {
 	while remaining > 0 {
 		let num     = num_from_slice(digits, i, 3);
 		let leading = myriad_number(num)?;
-		let zillion = zillion_number(remaining / 3 - 1, short)?;
+		let zillion = zillion_number(remaining / 3 - 1, scale)?;
 
 		if !leading.is_empty() {
 			if !output.is_empty() { output.push(' '); }
@@ -161,21 +188,20 @@ pub fn full_name(digits: &str, short: bool) -> Result<String, ParseError> {
 /// * `digits` - A string slice that holds a representation of the number
 /// using only the digits 0-9. If any other character is present, this function
 /// will return an Err.
-/// * `short` - A boolean to determine whether or not the short scale should
-/// be used. For reference, the represented by `10^9` is called "one billion"
-/// using the short scale. When `short` is set to false, the long scale is used
-/// instead, and this number would be called "one milliard".
+/// * `scale` - An enumerated value to determine which scale should
+/// be used. Short scales use a new "-illion" name for every power of 1000,
+/// while long scales use a new "-illion" name for every power of 1000000.
 /// 
 /// # Example
 ///
 /// ```
-/// use googology::conway_wechsler::power_of_ten;
-/// let milliard = power_of_ten("9", false).unwrap();
-/// let billion = power_of_ten("9", true).unwrap();
-/// assert_eq!("one milliard", milliard.as_str());
+/// use googology::conway_wechsler::{Scale, power_of_ten};
+/// let thousand_million = power_of_ten("9", Scale::LongBritish).unwrap();
+/// let billion = power_of_ten("9", Scale::Short).unwrap();
+/// assert_eq!("one thousand million", thousand_million.as_str());
 /// assert_eq!("one billion", billion.as_str());
 /// ```
-pub fn power_of_ten(digits: &str, short: bool) -> Result<String, ParseError> {
+pub fn power_of_ten(digits: &str, scale: Scale) -> Result<String, ParseError> {
 	// Sanity check. We want to convert our input string into a Bignum.
 	// The num_bigint crate doesn't quite allow us to know the cause of
 	// error, but from what we can tell, it's either an invalid digit or
@@ -214,25 +240,31 @@ pub fn power_of_ten(digits: &str, short: bool) -> Result<String, ParseError> {
 	// Compute zillion number.
 	power -= 1u32;
 	output.push_str(" ");
-	let loc = output.len(); // Location to insert prefixes at
 
 	// Adjust for long scale if necessary
-	let mut suffix = "on";
-	if !short {
-		suffix = if (&power % 2u32).is_zero() { "ard" } else { "on" };
+	let (prefix, suffix) = match (scale, (&power % 2u32).is_zero()) {
+		(Scale::Short, _) | (_, false) => ("", "on"),
+		(Scale::LongBritish, true)     => ("thousand ", "on"),
+		(Scale::LongPeletier, true)    => ("", "ard"),
+	};
+
+	if scale != Scale::Short {
 		power += 3u32;
 		power /= 2u32;
 		power -= 1u32;
 	}
 
-	// Add prefixes in reverse order because we are stupid and inefficient.
+	output.push_str(prefix);
+	let loc = output.len(); // Location to insert prefixes at
+
+	// Add zillions in reverse order because we are stupid and inefficient.
 	while !power.is_zero() {
-		let prefix = (&power % 1000u32)
+		let zillion = (&power % 1000u32)
 			.to_usize()
 			.ok_or(ParseError::InternalError)
 			.and_then(zillion_prefix)?;
 
-		output.insert_str(loc, prefix.as_str());
+		output.insert_str(loc, zillion.as_str());
 		power /= 1000u32;
 	}
 
@@ -246,37 +278,41 @@ mod tests {
 
 	#[test]
 	fn very_small_numbers() -> Result<(), ParseError> {
-		let zero_ss = full_name("0", true)?;
-		let zero_ls = full_name("0", false)?;
+		let zero_ss = full_name("0", Scale::Short)?;
+		let zero_lp = full_name("0", Scale::LongPeletier)?;
 		assert_eq!("zero", zero_ss.as_str());
-		assert_eq!("zero", zero_ls.as_str());
+		assert_eq!("zero", zero_lp.as_str());
 		Ok(())	
 	}
 
 	#[test]
 	fn small_numbers() -> Result<(), ParseError> {
-		let twelve_ss = full_name("12", true)?;
-		let twelve_ls = full_name("12", false)?;
+		let twelve_ss = full_name("12", Scale::Short)?;
+		let twelve_lb = full_name("12", Scale::LongBritish)?;
 		assert_eq!("twelve", twelve_ss.as_str());
-		assert_eq!("twelve", twelve_ls.as_str());
+		assert_eq!("twelve", twelve_lb.as_str());
 		Ok(())
 	}
 
 	#[test]
 	fn large_numbers() -> Result<(), ParseError> {
-		let billion = full_name("1000000000", true)?;
-		let milliard = full_name("1000000000", false)?;
-		assert_eq!("one milliard", milliard.as_str());
+		let billion = full_name("1000000000", Scale::Short)?;
+		let milliard = full_name("1000000000", Scale::LongPeletier)?;
+		let thousand_million = full_name("1000000000", Scale::LongBritish)?;
 		assert_eq!("one billion", billion.as_str());
+		assert_eq!("one milliard", milliard.as_str());
+		assert_eq!("one thousand million", thousand_million.as_str());
 		Ok(())
 	}
 
 	#[test]
 	fn large_powers() -> Result<(), ParseError> {
-		let googol_ss = power_of_ten("100", true)?;
-		let googol_ls = power_of_ten("100", false)?;
+		let googol_ss = power_of_ten("100", Scale::Short)?;
+		let googol_lb = power_of_ten("100", Scale::LongBritish)?;
+		let googol_lp = power_of_ten("100", Scale::LongPeletier)?;
 		assert_eq!("ten duotrigintillion", googol_ss.as_str());
-		assert_eq!("ten sedecilliard", googol_ls.as_str());
+		assert_eq!("ten thousand sedecillion", googol_lb.as_str());
+		assert_eq!("ten sedecilliard", googol_lp.as_str());
 		Ok(())
 	}
 }
